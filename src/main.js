8636e88de8bdbd75082b7eb559611b2c7c4d1600
David Pessanha <davidppessanha@gmail.com>
2021-09-21 10:42:42 -0300

    importing global component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseIcon from "@/components/BaseIcon.vue";
 
 Vue.config.productionTip = false;
 
