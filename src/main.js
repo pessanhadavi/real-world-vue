40d404b1a3829cf4bf0abbfdd75e8c6dcd7009e2
David Pessanha <davidppessanha@gmail.com>
2021-09-21 10:44:55 -0300

    Registered global component: BaseIcon

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import BaseIcon from "@/components/BaseIcon.vue";
 
+Vue.component("BaseIcon", BaseIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
