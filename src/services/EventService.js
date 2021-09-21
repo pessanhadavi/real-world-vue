8975f7c6410fe2b1836af6d902f0cd8022d3b6b6
David Pessanha <davidppessanha@gmail.com>
2021-09-21 15:20:15 -0300

    Reorganized code. Gathered all axios code in one place (EventService). That way it is only needed to create one axios instance for a modular code

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  // Single axios instance for our entire app
+  baseURL: "http://localhost:3000/", // Base URL for all calls to use
+  withCredentials: false,
+  headers: {
+    // For authentication and configuration to comunicate to our server
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getEvents() {
+    return apiClient.get("/events"); // Get request calling events
+  },
+};
