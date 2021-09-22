c36e16e641a2ac3347834a67dec5e18655e442d8
David Pessanha <davidppessanha@gmail.com>
2021-09-22 11:31:55 -0300

    config states and getters

--- src/store/index.js
+++ src/store/index.js
@@ -15,6 +15,12 @@ export default new Vuex.Store({
       "food",
       "community",
     ],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false },
+    ],
   },
   mutations: {},
   actions: {},
@@ -22,6 +28,13 @@ export default new Vuex.Store({
   getters: {
     catLength: (state) => {
       return state.categories.length;
+    },
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done);
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length;
+      // return state.todos.filter((todo) => !todo.done).length; -> This is an easier way (no getters)
     },
   },
 });
