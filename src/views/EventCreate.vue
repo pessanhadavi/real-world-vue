7897fb0c6e522d680958de898f55c6bc48c824f0
David Pessanha <davidppessanha@gmail.com>
2021-09-22 12:12:36 -0300

    Made some adjustments and registered datepicker as a component of eventCreate

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,49 +1,16 @@
 <template>
   <div>
-    <h1>Create an Event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.id }}</p>
-    <p>{{ getEventById(2) }}</p>
-    <!-- <p>There are {{ catLength }} categories</p>
-    <ul>
-      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
-    </ul> -->
+    <h1>Create an Event</h1>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import Datepicker from "vuejs-datepicker"
 
 export default {
-  computed: {
-    // getEvent() { The code below is a simpler form of this one
-    //   return this.$store.getters.getEventById;
-    // },
-    ...mapGetters(["getEventById"]),
-    // catLength() {
-    //   return this.$store.getters.catLength;
-    // },
-    ...mapState(["user", "categories"]),
-    // The code above is a simpler form of the below one
-    // ({ user: "user", If it is a top level state (no dots), you can use this notation
-    // categories: (state) => state.categories,})
-
-    /* If we need to access different parts of our State
-      from the same component, it can get verbose and
-      repetitive to have multiple computed properties
-      each returning this.$store.state.something.
-      To simplify things, we can use the mapState helper.
-      In this case, the code above replaces the one below.
-
-      computed: {
-        userName() {
-          return this.$store.state.user.name;
-        },
-        userID() {
-          return this.$store.state.user.id;
-        },
-      },
-    */
-  },
+  components: {
+    Datepicker
+  }
 };
 </script>
 
